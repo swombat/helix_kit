9df9d8a23117da45adc48c7265b5bd231f805b3a
swombat <daniel.github@tenner.org>
2025-08-28 10:54:45 +0200

    Test method -> remove

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,4 @@ class PagesController < ApplicationController
     render inertia: "Home"
   end
 
-  def create_flash
-    flash[:notice] = "This is a notice"
-    flash[:alert] = "This is an alert"
-    flash[:error] = "This is an error"
-    redirect_to root_path
-  end
-
 end
