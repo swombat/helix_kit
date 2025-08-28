879dcb5a79cc7f8384c413918c26c4d8db1f41c1
swombat <daniel.github@tenner.org>
2025-08-28 12:59:07 +0200

    ID obfuscation

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ gem "vite_rails", "~> 3.0"
 gem "bcrypt", "~> 3.1"
 
 gem "js-routes", "~> 2.3"
+
+gem "hashids"
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
 
+  include ObfuscatesId
+
   primary_abstract_class
 
 end
