945660081bd6ceef8a7b915155114457abd65248
swombat <daniel.github@tenner.org>
2025-08-28 09:13:44 +0200

    HelixKIT logo

--- app/frontend/lib/components/logo.svelte
+++ app/frontend/lib/components/logo.svelte
@@ -1,22 +1,14 @@
 <script>
-  import helixLogoSrc from "../../../assets/images/helix-logo.svg?raw";
+  import helixLogoSrc from '../../../assets/images/helix-kit-logo.svg?raw';
   let props = $props();
 
   // Extract props for the SVG element
-  const {
-    width = 84,
-    height = 84,
-    class: className = "",
-    ...restProps
-  } = props;
+  const { width = 84, height = 84, class: className = '', ...restProps } = props;
 
   // Convert restProps to HTML attributes
   const restAttributes = Object.entries(restProps)
     .map(([key, value]) => `${key}="${value}"`)
-    .join(" ");
+    .join(' ');
 </script>
 
-{@html helixLogoSrc.replace(
-  "<svg",
-  `<svg width="${width}" height="${height}" class="${className}" ${restAttributes}`,
-)}
+{@html helixLogoSrc.replace('<svg', `<svg width="${width}" height="${height}" class="${className}" ${restAttributes}`)}
