d82cd9e24169eef37a00a3d8279ddf426b5ae12a
swombat <daniel.github@tenner.org>
2025-09-20 20:52:49 +0200

    AR monkey patch only needs to be in dev

--- config/initializers/active_record_monkey_patches.rb
+++ config/initializers/active_record_monkey_patches.rb
@@ -1,11 +1,13 @@
-class ActiveRecord::Base
+if Rails.env.development?
+  class ActiveRecord::Base
 
-  def dump_fixture
-    fixture_file = "#{Rails.root}/test/fixtures/#{self.class.table_name}.yml"
-    File.open(fixture_file, "a+") do |f|
-      f.puts({ "#{self.class.table_name.singularize}_#{id}" => attributes }.
-        to_yaml.sub!(/---\s?/, "\n"))
+    def dump_fixture
+      fixture_file = "#{Rails.root}/test/fixtures/#{self.class.table_name}.yml"
+      File.open(fixture_file, "a+") do |f|
+        f.puts({ "#{self.class.table_name.singularize}_#{id}" => attributes }.
+          to_yaml.sub!(/---\s?/, "\n"))
+      end
     end
-  end
 
+  end
 end
