3babbec30f4b5a1d8882e1331ebea750e4f6a624
swombat <daniel.github@tenner.org>
2025-09-21 23:30:26 +0200

    Changed domain for mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "granttree.co.uk" }
+  config.action_mailer.default_url_options = { host: "helix-kit.granttree.co.uk" }
 
   if Rails.application.credentials.dig(:mailgun).present?
     config.action_mailer.delivery_method = :smtp
